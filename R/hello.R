5d6f5ad4fa374b82c8d2838b582d60c75283cb3e
London <Mark.London@ext.ons.gov.uk>
2025-01-10 13:36:28 +0000

    asdfr

---
+++ R/hello.R
@@ -0,0 +1,12 @@
+#' @title hello
+#' @description A happy greeting
+#' @author Mark London \email{mark.london@ext.ons.gov.uk}
+#' @details This is an example function named 'hello'
+#'  which prints 'Hello, world!'.
+#' @param NONE description
+#' @return None
+#' @example hello()
+#' @export
+hello <- function() {
+  print("Hello, world!")
+}
